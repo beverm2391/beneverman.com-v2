fe9d13131ed31ebc41b9c3108e9146246969205b
beverm2391 <beverman4528@gmail.com>
2022-07-15 22:10:34 -0400

    added a temp meta description, sitewide

--- components/layout.js
+++ components/layout.js
@@ -2,6 +2,7 @@ import Footer from './footer.js';
 import FadeIn from './animations/fadein.js';
 import Head from 'next/head';
 import NavbarResponsive from './navbarresponsive.js';
+import { NextSeo } from 'next-seo';
 
 const backgrounds = ['backgrounddm', 'gradientbackgrounddm', 'gradientbackgroundlm']
 const defaultbackground = 'backgrounddm'
@@ -12,6 +13,7 @@ export default function Layout({ title, children, background, footerclass, hiden
             <Head>
                 <link rel="icon" href="/favicon.png" />
                 <title>{title}</title>
+                <meta name="description" content="I'm a social work student, MBA student, and a developer. Looking to make a significant impact within mental healthcare." />
             </Head>
             <div className={background ? background : defaultbackground}>
                 {hidenavbar !== true && <NavbarResponsive navbarclass = {navbarclass}/>}
